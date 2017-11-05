e1312e7bc1dca875177fd099728671d546e109c1
lyubomyr-wilde <lyubomyrwilde@gmail.com>
2017-11-06 00:31:15 +0200

    Initial Commit

---
+++ source/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace hairdresser_s_notes
+{
+    [Activity(Label = "hairdresser_s_notes", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
